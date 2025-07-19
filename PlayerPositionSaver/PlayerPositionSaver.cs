d91f9cafb8e12d311ce1e61782cbf754a212a36a
shadow1117 <shadow1117@hotmail.de>
2025-07-19 20:05:33 +0200

    Initial Commit
    spiro made me do this

---
+++ PlayerPositionSaver/PlayerPositionSaver.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Persistence;
+using VRC.SDKBase;
+
+public class PlayerPositionSaver : UdonSharpBehaviour
+{
+    [Header("Interval in seconds between save ticks for each player.", order = 1)]
+    public float TimeBetweenSaves = 60.0f;
+    [Header("How many minutes the position gets remembered from the last time they left until they spawn at spawn again (0 = infinite).", order = 2)]
+    public int RememberTime = 120;
+
+
+    private float _elapsedTime = 0.0f;
+    private VRCPlayerApi MyPlayer; //More efficient then getting the LocalPlayer each time
+
+    void Start()
+    {   //More efficient then getting the LocalPlayer each time
+        MyPlayer = Networking.LocalPlayer;
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+        //Has the given interval time been passed?
+        if (_elapsedTime >= TimeBetweenSaves)
+        {
+            UpdatePlayerData();
+            _elapsedTime = 0.0f;
+        }
+    }
+
+    private void UpdatePlayerData()
+    {
+        //Runs for each player them self
+        if(MyPlayer != null && MyPlayer.IsValid())
+        {
+            PlayerData.SetQuaternion("Rotation", MyPlayer.GetRotation());
+            PlayerData.SetVector3("Position", MyPlayer.GetPosition());
+            PlayerData.SetLong("LastTime", DateTime.Now.Ticks);
+        }
+    }
+    
+    public override void OnPlayerRestored(VRCPlayerApi Player)
+    { //the funny new vrc function that makes this possible
+        if (Player.isLocal) //only teleport ourself 
+        { // we were here before?
+            if (PlayerData.HasKey(Player, "LastTime"))
+            { //does the time matter? 
+                TimeSpan elapsedSpan = new TimeSpan(DateTime.Now.Ticks - PlayerData.GetLong(Player, "LastTime"));
+                if (elapsedSpan.Minutes < RememberTime || RememberTime == 0)
+                {  //put them where they were
+                    Player.TeleportTo(PlayerData.GetVector3(Player, "Position"), PlayerData.GetQuaternion(Player, "Rotation"));
+                }
+            
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# VRC_Udon_PlayerPositionSaver
+Saves the position of a player in your world so if they leave for whatever reason and they come back they are at the same position where they left in. 
+For example in a sleeping world so that if they chrash or have a disconnect at night they are still with their friends. 
+Or in a club just to take that little anoyance of running back to your spot.
+Saving interval and the length of time the position is used instead of spawn are configurable.
+
+
+## To setup
+- Import the Package into your Project
+- Drag the Prefab into the root of your scene
+- modify the two variables to your liking
